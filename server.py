8456bc147d4ccef61da3a4656252baeddf92b851
qilyas <ilias.qamar@gmail.com>
2020-03-16 02:38:36 -0700

    Comments added

--- server.py
+++ server.py
@@ -3,11 +3,16 @@ import socket
 s= socket.socket()
 
 print("Socket Created!!")
+
+# After creating socket 3 steps.(Bind, listen and accept)
+#1. Bind
 s.bind(('localhost',9998))
+#2. listen
 s.listen(3)
 print("Waiting for connection")
 
 while True:
+    #3. accept
     c,addr=s.accept()
     name=c.recv(1024).decode()
     print(name)
