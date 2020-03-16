2eb9d5d3e8a28014754a7876133b196cd253836b
qilyas <ilias.qamar@gmail.com>
2020-03-16 00:22:22 -0700

    Added comments

--- mergeTheTools.py
+++ mergeTheTools.py
@@ -1,8 +1,8 @@
 def merge_the_tools(string, k):
 
-    for i in range(0,len(string),k):
+    for i in range(0,len(string),k): # iterate over i with index 0 to avery k
         s = ""
-        for j in string[i:i+k]:
+        for j in string[i:i+k]: # iterate over each k characters
             if j not in s:
                 s+=j
         print(s)
