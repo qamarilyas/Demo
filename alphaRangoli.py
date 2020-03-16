9da344740d4a3577389c73f6e4efa579e83b1f88
qilyas <ilias.qamar@gmail.com>
2020-03-15 19:53:23 -0700

    alphaRangoli

--- alphaRangoli.py
+++ alphaRangoli.py
@@ -1,11 +1,27 @@
 
-import string
 
-alpha=string.ascii_lowercase
-n= int(input())
-width=4*n-3
-L=[]
-for i in range(n):
-    ch="-".join(alpha[i:n])
-    L.append((ch[::-1]+ch[1:]).center(width,"-"))
-print("\n".join(L[::-1]+L[1:]))
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    for x in s.split():
+        s=s.replace(x,x.capitalize())
+    return s
+
+#if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+s = input()
+
+result = solve(s)
+print(result)
+
+    #fptr.write(result + '\n')
+
+    #fptr.close()
