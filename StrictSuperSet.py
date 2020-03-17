90daaaf743c0e5b86e091ccfabba7d072760a070
qilyas <ilias.qamar@gmail.com>
2020-03-17 00:25:30 -0700

    Strict Super Set added comments

--- StrictSuperSet.py
+++ StrictSuperSet.py
@@ -1,9 +1,9 @@
-A=set(input().split())
-n= int(input())
+A=set(input().split()) # Set A defined
+n= int(input()) # Len of the input of subsets
 for _ in range(n):
-    if not A.issuperset(set(input().split())):
+    if not A.issuperset(set(input().split())): # VERIFY that A is not superset of any of it
         print ("False")
         break
-else:
+else: # True if A is superset of all
     print("True")
 
