8ea7f6ca70e3104d1d7c9c5184ccf0972e015019
qilyas <ilias.qamar@gmail.com>
2020-03-18 01:42:33 -0700

    intertool product

---
+++ itertools.products.py
@@ -0,0 +1,7 @@
+#from intertools import product
+from itertools import product
+A=sorted(list(map(int,input().split())))
+B=sorted(list(map(int,input().split())))
+
+for i in product(A,B):
+    print(i,end=" ")
