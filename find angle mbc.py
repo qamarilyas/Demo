6725d3759ec9b0b5b5c5b1382ab95bf6980d19ad
qilyas <ilias.qamar@gmail.com>
2020-04-09 10:22:32 -0700

    find angle

--- find angle mbc.py
+++ find angle mbc.py
@@ -2,7 +2,7 @@ import math
 ab=int(input())
 bc=int(input())
 
-x=round(math.degrees(math.atan2(ab,bc)))
+x=round(math.degrees(math.atan2(ab,bc)))  #calculate the theta  using atan2 and degree
 print(x)
 
 #print(str(int(c*180/cmath.pi))+'°')
